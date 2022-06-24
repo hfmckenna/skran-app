6c9e890dfb687e4cf376b6ac71a5448210d7816b
hfmckenna <hfmckenna@outlook.com>
2022-06-24 12:02:29 +0100

    Add scaffold for React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 import { useEffect, useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import './index.css';
 
-function App() {
+export function App() {
     return (
-        <div className='App'>
-            <Recipe />
+        <>
+            <div className='App'>
+                <div>
+                    <nav>
+                        <ul className='flex justify-evenly py-5'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/admin'>Admin</Link>
+                            </li>
+                            <li>
+                                <Link to='/recipe'>Recipe</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export function Home() {
+    return (
+        <div>
+            <h1 className='text-center'>Home</h1>
         </div>
     );
 }
 
-function Recipe() {
+export function Admin() {
+    return (
+        <div>
+            <h1 className='text-center'>Admin</h1>
+        </div>
+    );
+}
+
+export function Recipe() {
     const [recipe, setRecipe] = useState<any>(null);
 
     useEffect(() => {
@@ -19,10 +53,10 @@ function Recipe() {
                     '/api/v1/recipe/622cd36f70a4d852b7dcbd55'
             ).then((res) => res.json());
             setRecipe(recipe);
-        })()
+        })();
     }, []);
 
-    return <div className="text-center">{recipe && recipe?.title}</div>;
+    return <div className='text-center'>{recipe && recipe?.title}</div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home, Recipe, Admin } from './App';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />}>
+                    <Route index element={<Home />} />
+                    <Route path='recipe' element={<Recipe />} />
+                    <Route path='admin' element={<Admin />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
