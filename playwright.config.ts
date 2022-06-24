c5e34860df2719650d0f3a43b485e62720ac0012
hfmckenna <hfmckenna@outlook.com>
2022-06-24 12:02:29 +0100

    Add e2e testing boilerplate

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,27 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path:'.env.development' });
+}
 
 const config: PlaywrightTestConfig = {
-  use: {
-    trace: 'on-first-retry',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    use: {
+        trace: 'on-first-retry',
+        baseURL: process.env.VITE_API_AAD_APP_REDIRECT_URI || 'http://localhost:3000',
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
+    ],
 };
 
 export default config;
