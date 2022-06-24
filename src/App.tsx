0066354aebfb1ea80eaad77cd255d60152e694a7
hfmckenna <hfmckenna@outlook.com>
2022-06-24 12:02:29 +0100

    Add trivial recipe data fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <Recipe />
+        </div>
+    );
 }
 
-export default App
+function Recipe() {
+    const [recipe, setRecipe] = useState<any>(null);
+
+    useEffect(() => {
+        (async function getRecipe() {
+            const recipe = await fetch(
+                import.meta.env.VITE_API_URL +
+                    '/api/v1/recipe/622cd36f70a4d852b7dcbd55'
+            ).then((res) => res.json());
+            setRecipe(recipe);
+        })()
+    }, []);
+
+    return <div>{recipe && recipe?.title}</div>;
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
